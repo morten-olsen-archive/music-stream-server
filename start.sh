cd03e0a5a9fdaffa89aee9a50fe34a680278414a
Morten Olsen <morten@olsen.pro>
2020-01-26 23:03:55 +0100

    uptimized

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-avconv -f alsa -ac 2 -i hw:1,0 -acodec mp2  -f rtp rtp://239.1.1.1:1234
+ffmpeg -f alsa -ac 2 -i hw:1,0 -acodec libmp3lame -ar 44100 -timeout 2000 -f rtp rtp://239.1.1.1:1234?fifo_size=50000000 
